c574d2fb8fdf612113d2ede165011cc8cc946449
Pablo Chuquimia <i.pouk.19@gmail.com>
2025-08-11 11:07:21 -0400

    fix(pdf exporter)

--- lib/features/utils/pdf_exporter_web.dart
+++ lib/features/utils/pdf_exporter_web.dart
@@ -12,7 +12,8 @@ Future<void> exportClothesToPDF(List<Clothes> clothes) async {
       build: (context) => pw.Column(
         crossAxisAlignment: pw.CrossAxisAlignment.start,
         children: [
-          pw.Text('Listado de Prendas', style: pw.TextStyle(fontSize: 24)),
+          pw.Text('Listado de Prendas de Vestir',
+              style: pw.TextStyle(fontSize: 24)),
           pw.SizedBox(height: 21),
           ...clothes.map((item) => pw.Text(
               '• ${item.name} - Tipo: ${item.type}, Talla: ${item.size}, Cantidad: ${item.quantity}')),
